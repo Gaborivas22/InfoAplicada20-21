25d9db417b6cc6c05b5f37f0a147b113b6029fd3
Gabriel Rivas <65317629+Gaborivas22@users.noreply.github.com>
2024-01-05 12:35:13 -0600

    Se agrego el archivo reto 2

---
+++ Programa de control de elevadores/Reto2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+Este programa fue creado por gabriel rivas 
+si lo copias recuerda borrar este cacho o cambiale jejej
+*/
+int nivel;
+int main() { 
+	printf("Programa de control de elevadores\n");
+	printf("Dame el nivel donde se encuentra el elevador\n");
+	scanf("%i",&nivel);
+	return 0;
+}
