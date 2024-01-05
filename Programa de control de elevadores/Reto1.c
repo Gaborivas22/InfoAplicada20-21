050525400a0c4ebcde2f172f32b0be3b77366ea4
Gabriel Rivas <65317629+Gaborivas22@users.noreply.github.com>
2024-01-04 18:56:01 -0600

    Se agrego el primer Reto

---
+++ Programa de control de elevadores/Reto1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+Este programa fue creado por gabriel rivas 
+*/
+int main() {
+	printf("Programa de control de elevadores\n");	//especificamos el nombre que mostrara el programa mediante el comando printf
+	return 0;
+}
+
